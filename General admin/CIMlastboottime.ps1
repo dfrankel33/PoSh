65b1279f4b83eed7e4d5734fc51848bb5723fcfd
dfrankel33 <davidfrankel@RS1155.local>
2015-12-05 16:13:45 -0500

    2

--- General admin/CIMlastboottime.ps1
+++
@@ -1,8 +0,0 @@
-﻿
-$servers = @()
-
-foreach ($server in $servers) {
-    $session = New-CimSession -ComputerName $server -Credential $admin
-    $CIM = Get-CimInstance -CimSession $session -ClassName win32_operatingsystem
-    $CIM | select CSName,LastBootUpTime
-}
