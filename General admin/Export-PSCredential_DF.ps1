7aedcf5d26c972d4424f9cccf09eae069ae2cfb8
dfrankel33 <davidfrankel@RS1155.local>
2015-12-05 16:17:03 -0500

    pscred exp/imp

---
+++ General admin/Export-PSCredential_DF.ps1
@@ -0,0 +1,38 @@
+﻿#Function to export credential to encrypted file
+#Edited to include $CredName parameter.  This will allow multiple different Credentials to be used.
+
+try {$ScriptPath = (Split-Path ((Get-Variable MyInvocation).Value).MyCommand.Path)} catch{}
+if (-not(test-path variable:\ScriptPath -ErrorAction SilentlyContinue)) {$ScriptPath = '.\'}
+function Export-PSCredential {
+	param ( $Credential = (Get-Credential), $CredName, $Credfile = $ScriptPath + "\$CredName.enc.xml")
+
+	# Look at the object type of the $Credential parameter to determine how to handle it
+	switch ( $Credential.GetType().Name ) {
+		# It is a credential, so continue
+		PSCredential		{ continue }
+		# It is a string, so use that as the username and prompt for the password
+		String				{ $Credential = Get-Credential -credential $Credential }
+		# In all other caess, throw an error and exit
+		default				{ Throw "You must specify a credential object to export to disk." }
+	}
+	
+	# Create temporary object to be serialized to disk
+	$export = "" | Select-Object Username, EncryptedPassword
+	
+	# Give object a type name which can be identified later
+	$export.PSObject.TypeNames.Insert(0,'ExportedPSCredential')
+	
+	$export.Username = $Credential.Username
+	
+
+	# Encrypt SecureString password using Data Protection API
+	# Only the current user account can decrypt this cipher
+	$export.EncryptedPassword = $Credential.Password | ConvertFrom-SecureString
+
+	# Export using the Export-Clixml cmdlet
+	$export | Export-Clixml $Credfile
+	Write-Host -foregroundcolor Green "Credentials saved to: " -noNewLine
+
+	# Return FileInfo object referring to saved credentials
+	Get-Item $Credfile
+}
---
+++ General admin/Import-PSCredential_DF.ps1
@@ -0,0 +1,22 @@
+﻿#Function to import encrypted credential file
+
+function Import-PSCredential {
+        param ( $Path = "default.enc.xml" )
+ 
+        # Import credential file
+        $import = Import-Clixml $Path
+       
+        # Test for valid import
+        if ( !$import.UserName -or !$import.EncryptedPassword ) {
+                Throw "Input is not a valid ExportedPSCredential object, exiting."
+        }
+        $Username = $import.Username
+       
+        # Decrypt the password and store as a SecureString object for safekeeping
+        $SecurePass = $import.EncryptedPassword | ConvertTo-SecureString
+       
+        # Build the new credential object
+        $Credential = New-Object System.Management.Automation.PSCredential $Username, $SecurePass
+        Write-Output $Credential
+}
+Import-PSCredential
